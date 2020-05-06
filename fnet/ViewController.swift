3cdbb8afb40fff0d2a0271a587e5c054e5e96290
Georges Nomicos <georgesnomicos@gmail.com>
2020-05-06 23:19:53 +0200

    Initial Commit

---
+++ fnet/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  fnet
+//
+//  Created by Georges on 06/05/2020.
+//  Copyright © 2020 Nomicos. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
